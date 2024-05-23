134a824ecdb71cd626a14e450547a6ea40b38ba0
Laia Rodriguez Ramos <lrodrigu22@jaumebalmes.net>
2024-05-23 16:45:32 +0200

    Comandes de carreto getCarreto, CarretoById, DeleteCarretoById

--- api_Store/carreto/views.py
+++ api_Store/carreto/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .serializer import *
+from .models import *
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(['GET'])
+def get_Carreto(request):
+    carretoList = Carreto.objects.all()
+    data_serializer = CarretoSerializer(carretoList, many=True)
+    return Response({"data":data_serializer.data})
+
+@api_view(['GET'])
+def get_Carreto_ById(request, pk):
+    carreto = Carreto.objects.get(id=pk)
+    data_serializer = CarretoSerializer(carreto, many=False)
+    return Response({"data":data_serializer.data})
+
+@api_view(['DELETE'])
+def delete_Carreto_ById(request, pk):
+    carreto= Carreto.objects.get(id=pk)
+    carreto.delete()
+    return Response({"message": "oleeeee eliminado"})
