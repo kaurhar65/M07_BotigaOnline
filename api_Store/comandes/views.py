fa32007cc7e974888c908791a85150ee0cd7d9ac
Laia Rodriguez Ramos <lrodrigu22@jaumebalmes.net>
2024-05-26 12:30:50 +0200

    messages comandes

--- api_Store/comandes/views.py
+++ api_Store/comandes/views.py
@@ -9,14 +9,13 @@ from rest_framework.response import Response
 def get_Comand(request):
     comandList = Comanda.objects.all()
     data_serializer = comandaSerializer(comandList, many=True)
-    return Response({"data":data_serializer.data})
+    return Response({"message": "Totes les comandes", "data":data_serializer.data})
 
 @api_view(['GET'])
 def get_Comand_ById(request, pk):
     comandList = Comanda.objects.get(id=pk)
     data_serializer = comandaSerializer(comandList, many=False)
-    return Response({"data":data_serializer.data})
-
+    return Response({"message": "Comandes segons l'ID", "data":data_serializer.data})
 
 @api_view(['GET'])
 def get_Comand_ByClient(request, client_id):
@@ -24,7 +23,7 @@ def get_Comand_ByClient(request, client_id):
         client = Client.objects.get(id=client_id)
         comandes = Comanda.objects.filter(client=client)
         data_serializer = comandaSerializer(comandes, many=True)
-        return Response({"data": data_serializer.data})
+        return Response({"message": "Comandes segons l'ID client", "data": data_serializer.data})
     except Client.DoesNotExist:
         return Response({"error": "Client no trobat"}, status=404)
     
@@ -32,10 +31,10 @@ def get_Comand_ByClient(request, client_id):
 def get_Comand_Active(request):
     comandes = Comanda.objects.filter(actiu=True)
     data_serializer = comandaSerializer(comandes, many=True)
-    return Response({"data": data_serializer.data})
+    return Response({"message": "Comandes que es troben actives", "data": data_serializer.data})
 
 @api_view(['DELETE'])
 def delete_Comand_ById(request, pk):
     comand= Comanda.objects.get(id=pk)
     comand.delete()
-    return Response({"message": "oleeeee eliminado"})
+    return Response({"message": "Comanda eliminada correctament."})
